2e1a372e1eb0532fe6315cf6076f4aec1231403e
@stevo.juhas <64430078+Stefan-creator-arch@users.noreply.github.com>
2026-02-20 22:02:49 +0100

    Upgrade Firebase SDK and change notification icon
    Updated Firebase SDK version and modified notification icon URL.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,36 +1,7 @@
-
-// Importovanie skriptov potrebných pre Service Workera
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
-// Tvoja konfigurácia z obrázka
-const firebaseConfig = {
-  apiKey: "AIzaSyAdhUjoq7szgFbp8WN5bTKx3...", 
-  authDomain: "stefanov-chat.firebaseapp.com",
-  projectId: "stefanov-chat",
-  storageBucket: "stefanov-chat.firebasestorage.app",
-  messagingSenderId: "915409159507",
-  appId: "1:915409159507:web:6150b7b7b91..."
-};
-
-// Inicializácia
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-// Spracovanie správ na pozadí (keď je prehliadač zatvorený)
-messaging.onBackgroundMessage((payload) => {
-  console.log('Prijatá správa na pozadí: ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/ikona.png' 
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
+// Tvoja konfigurácia pre Service Worker
 firebase.initializeApp({
   apiKey: "AIzaSyAdhUJoq7szgFbp8WN5bTKx3Ib6OR8Oh5o",
   authDomain: "stefanov-chat.firebaseapp.com",
@@ -41,12 +12,14 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
+// Spracovanie správy na pozadí (keď je karta zavretá)
 messaging.onBackgroundMessage((payload) => {
-  console.log('Notifikácia na pozadí:', payload);
+  console.log('Notifikácia na pozadí prijatá:', payload);
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/static/icon.png' // ak máš ikonku, zmeň cestu
+    icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968771.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
